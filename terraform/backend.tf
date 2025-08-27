e65c6b7a39ed9353e5fe6347f17cbb2410a9a734
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-27 12:58:16 +0100

    commiting README.md file

---
+++ terraform/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket       = "terraform-state-bucket-app-25-08-2025"
+    use_lockfile = true
+    region       = "eu-north-1"
+    key          = "aws-terraform-states/terraform.tfstate"
+    encrypt      = true
+  }
+
+}
