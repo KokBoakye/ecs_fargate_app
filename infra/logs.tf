093e7272a67779aa3bdcfa686773b5140c59b933
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-27 12:51:18 +0100

    commiting README.md file

--- infra/logs.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_cloudwatch_log_group" "app" {
-  name              = "${var.app_name}-${var.environment}"
-  retention_in_days = var.log_retention_in_days
-
-}
--- infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "alb_dns" {
-  value = aws_lb.load_balancer.dns_name
-}
-
-output "aws_ecr_repo" {
-  value = aws_ecr_repository.app_repo.repository_url
-}
