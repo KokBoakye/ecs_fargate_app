bf07e1ef98cae420c06c81fb5a10595151b1c582
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-27 01:21:39 +0100

    destroy resources

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-state-bucket-app-25-08-2025"
-    use_lockfile = true
+    use_lockfile = false
     region       = "eu-north-1"
     key          = "aws-terraform-states/terraform.tfstate"
     encrypt      = true
